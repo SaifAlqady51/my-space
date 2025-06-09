a265ea6bebacd6c5382eaff8197f3ee2490c70b2
SaifAlqady51 <saifalqady51@gmail.com>
2025-06-09 21:51:41 +0300

    feat: Created footer

---
+++ src/components/navigation/footer.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { NAVLIST } from "./data/nav-list";
+
+export const Footer = () => {
+  return (
+    <div className="flex justify-between sticky bottom-0 shadow-black shadow-2xl  md:hidden w-full">
+      {NAVLIST.map((item, idx) => (
+        <Link
+          href={item.link}
+          key={idx}
+          className="flex items-center justify-center p-4 transition-colors"
+        >
+          <div className="flex items-center gap-0.5 flex-col hover:bg-black ">
+            <div className="text-2xl">{item.icon}</div>
+            <p className="text-xs">{item.name}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
