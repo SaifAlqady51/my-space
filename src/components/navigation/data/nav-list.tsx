f86f8f347f5b42c3c80106d962a3b7d1ee81adcc
SaifAlqady51 <saifalqady51@gmail.com>
2025-06-09 21:51:48 +0300

    feat: Created nav-list

---
+++ src/components/navigation/data/nav-list.tsx
@@ -0,0 +1,9 @@
+import { IoMdHome, IoMdInformation, IoIosCall } from "react-icons/io";
+import { IoInformationOutline } from "react-icons/io5";
+import { IoMdCall } from "react-icons/io";
+import { FaInfo } from "react-icons/fa6";
+export const NAVLIST = [
+  { name: "Home", link: "/", icon: <IoMdHome /> },
+  { name: "About", link: "/about", icon: <FaInfo /> },
+  { name: "Contact", link: "/contact", icon: <IoMdCall /> },
+];
