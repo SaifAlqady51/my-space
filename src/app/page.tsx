923a58418288df7a250e8363bce2c45511219bfb
SaifAlqady51 <saifalqady51@gmail.com>
2025-06-09 21:51:18 +0300

    refactor: Added Welcome Transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import WelcomeTransition from "@/components/welcome-transition";
+
 export default function Main() {
   return (
-    <main>
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold mb-4">Welcome to the Main Page</h1>
-        <p className="text-lg text-gray-700">This is the main content area.</p>
-      </div>
-    </main>
+    <>
+      <WelcomeTransition />
+      <main className="">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold mb-4 hover:text-black">
+            Welcome to the Main Page
+          </h1>
+        </div>
+      </main>
+    </>
   );
 }
