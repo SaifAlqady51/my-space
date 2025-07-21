b28706dfec2f694a8db1eb9f42a49c76fe47ef3e
SaifAlqady51 <saifalqady51@gmail.com>
2025-07-21 19:50:33 +0300

    feat: Created black-hole component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,36 @@ module.exports = {
       colors: {
         primary: "#1d4e89", // Direct color assignment
       },
+      animation: {
+        "fade-in": "fadeIn 1s ease-in-out",
+        "slide-up": "slideUp 0.8s ease-out",
+        "scale-in": "scaleIn 0.5s ease-out",
+      },
+      fontSize: {
+        xxs: "0.625rem",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        slideUp: {
+          "0%": { transform: "translateY(20px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        scaleIn: {
+          "0%": { transform: "scale(0.8)", opacity: "0" },
+          "100%": { transform: "scale(1)", opacity: "1" },
+        },
+        boxShadow: {
+          top: "0 -1px 3px 0 rgb(0 0 0 / 0.1), 0 -1px 2px -1px rgb(0 0 0 / 0.1)",
+          "top-md":
+            "0 -4px 6px -1px rgb(0 0 0 / 0.1), 0 -2px 4px -2px rgb(0 0 0 / 0.1)",
+          "top-lg":
+            "0 -10px 15px -3px rgb(0 0 0 / 0.1), 0 -4px 6px -4px rgb(0 0 0 / 0.1)",
+          "top-2xl": "0 -25px 50px -12px rgba(0, 0, 0, 0.25)",
+        },
+      },
     },
   },
   plugins: [],
