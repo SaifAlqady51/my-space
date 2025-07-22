3bdaceb66007def573a3314a02ff87044feae3bf
SaifAlqady51 <saifalqady51@gmail.com>
2025-07-22 22:38:42 +0300

    refactor: Edited main page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import WelcomeTransition from "@/components/welcome-transition";
-
 export default function Main() {
   return (
-    <>
-      <WelcomeTransition />
-      <main className="">
-        <div className="flex flex-col items-center justify-center h-screen">
-          <h1 className="text-4xl font-bold mb-4 hover:text-black">
-            Welcome to the Main Page
-          </h1>
-        </div>
-      </main>
-    </>
+    <main className="">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-4 hover:text-xs">
+          Welcome to the Main Page
+        </h1>
+      </div>
+    </main>
   );
 }
