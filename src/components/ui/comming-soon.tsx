7516142b91d3028dcd97496522919ca74b02366f
SaifAlqady51 <saifalqady51@gmail.com>
2025-07-26 22:44:58 +0300

    feat: Creating comming-soon page

---
+++ src/components/ui/comming-soon.tsx
@@ -0,0 +1,9 @@
+const CommingSoon = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <h1 className="text-4xl font-bold text-primary">Coming Soon</h1>
+    </div>
+  );
+};
+
+export default CommingSoon;
