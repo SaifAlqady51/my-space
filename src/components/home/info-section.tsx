806020665b6b3952aac7921e971fc92f4d30e0c9
SaifAlqady51 <saifalqady51@gmail.com>
2025-07-26 22:42:11 +0300

    refactor: editing info-section

--- src/components/home/info-section.tsx
+++ src/components/home/info-section.tsx
@@ -1,4 +1,3 @@
-// components/ui/info-section.tsx
 import Image, { StaticImageData } from "next/image";
 import AnimatedText from "@/components/ui/animated-text";
 import React, { ReactNode } from "react";
@@ -35,16 +34,15 @@ const InfoSection: React.FC<InfoSectionProps> = ({
     <div className="flex justify-center items-center h-screen w-full">
       <div
         className={cn(
-          "flex flex-col md:flex-row md:items-start items-center md:justify-center justify-start h-screen md:py-16 py-10 md:gap-8 space-y-8 px-4 md:px-48",
+          "flex flex-col md:flex-row md:items-start items-center md:justify-center justify-start h-screen md:gap-8 gap-4",
           containerClassName,
         )}
       >
-        {/* Text Content - order controlled by DOM position */}
         <div
           className={cn(
-            "flex flex-col md:items-start items-center justify-start gap-8 md:h-full h-fit ",
-            textContainerClassName,
+            "flex flex-col md:items-start items-center justify-start gap-8 md:h-full h-fit",
             imagePosition === "left" ? "md:order-2" : "md:order-1",
+            textContainerClassName,
           )}
         >
           <AnimatedText
@@ -53,20 +51,23 @@ const InfoSection: React.FC<InfoSectionProps> = ({
           >
             {title}
           </AnimatedText>
-          <AnimatedText
-            delay={animationDelay + 0.6}
-            animationType="fadeUp"
-            className="md:text-3xl text-center md:text-start text-lg font-light "
-          >
-            {description}
-          </AnimatedText>
+
+          {subTitle && (
+            <AnimatedText
+              delay={animationDelay + 0.6}
+              animationType="fadeUp"
+              className="md:text-2_5xl text-center md:text-start text-lg font-light"
+            >
+              {subTitle}
+            </AnimatedText>
+          )}
 
           <AnimatedText
-            delay={animationDelay + 0.6}
+            delay={animationDelay + 1}
             animationType="fadeUp"
-            className="md:text-lg text-center md:text-start text-sm font-light "
+            className="md:text-lg text-center md:text-start text-sm font-light"
           >
-            {subTitle}
+            {description}
           </AnimatedText>
         </div>
 
