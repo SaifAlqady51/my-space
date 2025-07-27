94e3e718aa630390a3d9dc440052e27f31f4d684
SaifAlqady51 <saifalqady51@gmail.com>
2025-07-27 20:59:06 +0300

    ui: Edited info-section

--- src/components/home/info-section.tsx
+++ src/components/home/info-section.tsx
@@ -3,7 +3,7 @@ import AnimatedText from "@/components/ui/animated-text";
 import React, { ReactNode } from "react";
 import { cn } from "@/lib/utils";
 
-interface InfoSectionProps {
+export interface InfoSectionProps {
   imageSrc: string | StaticImageData;
   imageAlt: string;
   imageWidth?: number;
@@ -31,57 +31,55 @@ const InfoSection: React.FC<InfoSectionProps> = ({
   imageClassName = "",
 }) => {
   return (
-    <div className="flex justify-center items-center h-screen w-full">
+    <div
+      className={cn(
+        "flex flex-col xl:flex-row xl:items-start items-center xl:justify-center justify-start  xl:gap-8 gap-4  h-fit w-full",
+        containerClassName,
+      )}
+    >
       <div
         className={cn(
-          "flex flex-col md:flex-row md:items-start items-center md:justify-center justify-start h-screen md:gap-8 gap-4",
-          containerClassName,
+          "flex flex-col xl:items-start items-center justify-start gap-8 xl:h-full h-fit max-w-[750px]",
+          imagePosition === "left" ? "xl:order-2" : "xl:order-1",
+          textContainerClassName,
         )}
       >
-        <div
-          className={cn(
-            "flex flex-col md:items-start items-center justify-start gap-8 md:h-full h-fit",
-            imagePosition === "left" ? "md:order-2" : "md:order-1",
-            textContainerClassName,
-          )}
+        <AnimatedText
+          animationType="fadeUp"
+          className="text-title xl:text-left text-center font-semibold  xl:mt-20 mt-0"
         >
+          {title}
+        </AnimatedText>
+
+        {subTitle && (
           <AnimatedText
+            delay={animationDelay + 0.6}
             animationType="fadeUp"
-            className="md:text-5xl text-2xl md:text-left text-center font-semibold leading-snug whitespace-pre md:mt-20 mt-0"
+            className="xl:text-2_5xl md:text-2xl text-center xl:text-start text-lg font-light"
           >
-            {title}
+            {subTitle}
           </AnimatedText>
+        )}
 
-          {subTitle && (
-            <AnimatedText
-              delay={animationDelay + 0.6}
-              animationType="fadeUp"
-              className="md:text-2_5xl text-center md:text-start text-lg font-light"
-            >
-              {subTitle}
-            </AnimatedText>
-          )}
-
-          <AnimatedText
-            delay={animationDelay + 1}
-            animationType="fadeUp"
-            className="md:text-lg text-center md:text-start text-sm font-light"
-          >
-            {description}
-          </AnimatedText>
-        </div>
-
-        <Image
-          src={imageSrc}
-          alt={imageAlt}
-          width={imageWidth}
-          className={cn(
-            "rounded-2xl max-md:h-[400px] max-md:object-cover max-md:object-bottom md:rounded-none md:h-auto md:object-contain",
-            imagePosition === "left" ? "md:order-1" : "md:order-2",
-            imageClassName,
-          )}
-        />
+        <AnimatedText
+          delay={animationDelay + 1}
+          animationType="fadeUp"
+          className="xl:text-lg md:text-md text-center xl:text-start text-sm font-light"
+        >
+          {description}
+        </AnimatedText>
       </div>
+
+      <Image
+        src={imageSrc}
+        alt={imageAlt}
+        width={imageWidth}
+        className={cn(
+          "rounded-2xl max-xl:h-[400px] max-xl:object-cover max-xl:object-bottom xl:rounded-none xl:h-auto xl:object-contain",
+          imagePosition === "left" ? "xl:order-1" : "xl:order-2",
+          imageClassName,
+        )}
+      />
     </div>
   );
 };
