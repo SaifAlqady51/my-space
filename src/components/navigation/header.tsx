17454ce36b5bd3b84ce1c000487ff0daee5ae533
SaifAlqady51 <saifalqady51@gmail.com>
2025-07-29 19:05:51 +0300

    ui: Edited header component ui

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -5,7 +5,7 @@ import { NAVLIST } from "@/data";
 
 export const Header = () => {
   return (
-    <div className="w-full gap-4 h-16 bg-slate-950 border-b border-white/10 flex md:justify-between justify-center shadow-lg lg:px-32 xl:px-[var(--main-px)] px-8 top-0 z-50">
+    <div className="w-full gap-4 h-16 bg-slate-950 border-b border-white/10 flex md:justify-between justify-center shadow-gray-800 shadow-md  lg:px-32 xl:px-[var(--main-px)] px-8 top-0 z-50">
       <Link className="flex items-center" href="/">
         <OwlIcon />
         <h1 className="text-2xl font-bold whitespace-nowrap">Safe Space</h1>
@@ -15,7 +15,7 @@ export const Header = () => {
           <Link
             href={item.link}
             key={idx}
-            className="hover:scale-105 transition-transform hover:text-white duration-300"
+            className="hover:scale-105 transition-transform hover:text-gold-100 duration-200"
           >
             {item.name}
           </Link>
